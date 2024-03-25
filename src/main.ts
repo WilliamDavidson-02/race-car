883536be4076b75cc9c9f9244b6670b209924041
William Davidson <wj.davidson@outlook.com>
2024-03-25 10:48:09 +0100

    added raycaster vehicle

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import GUI from "lil-gui";
 
 import { updateAllMaterials } from "./utils.js";
 import { DebugObjType } from "./types.js";
+import { car, createCar, moveCar, wheels } from "./car.js";
+import { createGround, world } from "./world.js";
 
 const canvas = document.querySelector("canvas");
 
@@ -54,24 +57,18 @@ rgbeLoader.load("/sky.hdr", (environmentMap) => {
 /**
  * Models
  */
-gltfLoader.load("/models/car.glb", (car) => {
-  scene.add(car.scene);
+gltfLoader.load("/models/car.glb", (gltf) => {
+  scene.add(gltf.scene);
+  createCar(gltf.scene);
 
   updateAllMaterials(scene, debugObj);
-  camera.lookAt(car.scene.position);
+  camera.lookAt(gltf.scene.position);
 });
 
 /**
- * Meshes
+ * Ground
  */
-const ground = new THREE.Mesh(
-  new THREE.PlaneGeometry(250, 250),
-  new THREE.MeshStandardMaterial()
-);
-
-ground.rotation.x = -Math.PI * 0.5;
-
-scene.add(ground);
+createGround(scene);
 
 /**
  * Camera
@@ -92,6 +89,9 @@ directLight.castShadow = true;
 directLight.position.set(7, 6, 0.4);
 directLight.shadow.camera.far = 15;
 directLight.shadow.camera.near = 3;
+directLight.shadow.mapSize.set(1024, 1024);
+directLight.shadow.normalBias = 0.005;
+directLight.shadow.bias = -0.002;
 
 guiLights
   .add(directLight, "intensity", 0, 10, 0.001)
@@ -99,6 +99,8 @@ guiLights
 guiLights.add(directLight.position, "x", -10, 10, 0.001).name("directLight-X");
 guiLights.add(directLight.position, "y", -10, 10, 0.001).name("directLight-Y");
 guiLights.add(directLight.position, "z", -10, 10, 0.001).name("directLight-Z");
+guiLights.add(directLight.shadow, "normalBias", -0.05, 0.05, 0.001);
+guiLights.add(directLight.shadow, "bias", -0.05, 0.05, 0.001);
 
 // const directLightHelper = new THREE.CameraHelper(directLight.shadow.camera);
 
@@ -109,6 +111,7 @@ scene.add(directLight);
  */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas!,
+  antialias: true,
 });
 
 renderer.shadowMap.enabled = true;
@@ -116,9 +119,47 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
+ * Orbitcontrolls
+ */
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.enableZoom = false;
+controls.enableRotate = true;
+
+gui.add(controls, "enableRotate");
+
+/**
  * Animate
  */
+const clock = new THREE.Clock();
+let oldElapsedTime = 0;
+
 const animate = (): void => {
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - oldElapsedTime;
+  oldElapsedTime = deltaTime;
+
+  // Update physics world
+  world.step(1 / 60, deltaTime, 3);
+
+  if (car && car.gltf && car.vehicle) {
+    car.gltf.position.copy(car.vehicle.chassisBody.position);
+    car.gltf.quaternion.copy(car.vehicle.chassisBody.quaternion);
+
+    for (let i = 0; i < 4; i++) {
+      const wheel = car.vehicle.wheelInfos[i];
+      if (!wheel) continue;
+
+      car.vehicle.updateWheelTransform(i);
+      wheels[i].position.copy(
+        car.vehicle.wheelInfos[i].worldTransform.position
+      );
+      wheels[i].quaternion.copy(
+        car.vehicle.wheelInfos[i].worldTransform.quaternion
+      );
+    }
+  }
+
   renderer.render(scene, camera);
 
   window.requestAnimationFrame(animate);
@@ -128,6 +169,7 @@ const animate = (): void => {
  * Event listeners
  */
 window.addEventListener("DOMContentLoaded", animate);
+
 window.addEventListener("resize", () => {
   // Update sizes
   sizes.width = window.innerWidth;
@@ -141,3 +183,5 @@ window.addEventListener("resize", () => {
   renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
+
+window.addEventListener("keydown", ({ key }) => moveCar(key));
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,11 @@
+import { RaycastVehicle } from "cannon-es";
+import { Object3D } from "three";
+
 export type DebugObjType = {
   envMapIntensity: number;
+};
+
+export type CarType = {
+  gltf: Object3D | undefined;
+  vehicle: RaycastVehicle | undefined;
 };
